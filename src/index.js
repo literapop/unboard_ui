1e63385d5f051a958d832ea0475c203ee865bee7
Joe LoPresti <jlopresti87@gmail.com>
2019-06-15 15:04:05 -0400

    Render first query

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
-// import {Query, ApolloProvider} from 'react-apollo';
+import {ApolloProvider} from 'react-apollo';
 import gql from 'graphql-tag';
 // import {resolvers, typeDefs} from './resolvers';
 
@@ -50,7 +50,11 @@ client
   })
   .then(result => console.log(result));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+   <App /> 
+  </ApolloProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
