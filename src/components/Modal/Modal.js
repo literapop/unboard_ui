baa57da3cafa6b3ecfee0a39255048bd7d49de3b
ncgeary <ncgeary@buffalo.edu>
2019-06-15 11:33:29 -0400

    login and modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import LoginButtons from './loginbuttons';
+
+const Modal = () => (
+  <div id="cover">
+    <div id="logincard">
+      <div>
+        <div id="logo"><i className="fa fa-pinterest" aria-hidden="true" /></div>
+        <div id="welcome">Welcome to UnBoard.today</div>
+        <div id="subheader">Brought to you by your University</div>
+        <div id="gitsource">
+          <a target="_blank" rel="noopener noreferrer">
+            
+          </a>
+        </div>
+      </div>
+      <div>
+        <LoginButtons guest={() => {}} />
+      </div>
+    </div>
+  </div>
+);
+
+export default Cover;
