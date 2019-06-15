2086a98a703ecda147b7d98b045b014fbe55d0b1
ncgeary <ncgeary@buffalo.edu>
2019-06-15 11:52:58 -0400

    Switch to Login

--- src/components/Login/Modal.js
+++ src/components/Login/Modal.js
@@ -5,7 +5,8 @@ const Modal = () => (
   <div id="cover">
     <div id="logincard">
       <div>
-        <div id="logo"><i className="fa fa-pinterest" aria-hidden="true" /></div>
+        <div id="logo">UnBoard.Today</div>
+
         <div id="welcome">Welcome to UnBoard.today</div>
         <div id="subheader">Brought to you by your University</div>
         {/* <div id="gitsource">
@@ -21,4 +22,4 @@ const Modal = () => (
   </div>
 );
 
-export default Cover;
+export default Modal;
