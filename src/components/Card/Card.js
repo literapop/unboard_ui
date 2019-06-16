b0775f5edf2f7e357108268b4668ac9ab387445a
Michael Brennan <brennan3@gmail.com>
2019-06-16 01:55:25 -0400

    clean up warnings

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({imageUrl, activityName, type, toggleModal, activity}) => {
   return (
     <figure onClick={() => toggleModal(activity)}>
-      <img src={imageUrl.toString()} />
+      <img src={imageUrl.toString()} alt={activityName} />
       <figcaption>
         <p>{activityName}</p>
         <p>Type of Activity: {type}</p>
